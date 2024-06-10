f888fca33aea43441aea46a7cc7030243ca870e1
Wojciech <Bulbik1500@gmail.com>
2024-06-10 20:32:20 +0200

    added comments and extendet database

--- function/db.php
+++ function/db.php
@@ -1,4 +1,5 @@
 <?php
+// Data base info for use in diffrent files
 $DB_servername = "localhost";
 $DB_username = "root";
 $DB_password = "";
--- useroptions/userorders.php
+++ useroptions/userorders.php
@@ -13,8 +13,9 @@
     include "../function/db.php";
     $email = $_SESSION['email'];
     $conn = new mysqli($DB_servername, $DB_username, $DB_password, $DB_name);
-    $sql = 'SELECT orders.orderID, orders.email, orders.status, orders.description, orders.servicename, orders.cost , users.name FROM orders LEFT JOIN users ON orders.email = users.email
-    WHERE orders.email = "'.$email.'" ORDER BY orders.orderID DESC';
+    $sql = 'SELECT orders.orderID, orders.email, orders.status, orders.description, orders.servicename,users.name, services.cost FROM orders LEFT JOIN users ON orders.email = users.email LEFT JOIN services ON orders.servicename = services.servicename
+    WHERE orders.email = "'.$email.'"
+    ORDER BY orders.orderID DESC';
     $result = @$conn->query($sql);
 
     while ($row = $result->fetch_assoc()){
